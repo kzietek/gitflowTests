c0f57bec1224e9c2a84a162fb35cd42a1b818075
Kamil Ziętek <contact@kamilzietek.pl>
2015-05-10 20:51:18 +0200

    Created new XCode project

---
+++ AutolayoutTest/AutolayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutTest
+//
+//  Created by Kamil Zietek on 10.05.2015.
+//  Copyright (c) 2015 Kamil Zietek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutolayoutTest/AutolayoutTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutolayoutTest
+//
+//  Created by Kamil Zietek on 10.05.2015.
+//  Copyright (c) 2015 Kamil Zietek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
