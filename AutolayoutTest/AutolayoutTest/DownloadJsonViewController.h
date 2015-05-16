349dbaa05fc80cddf47939053c7e68488da3f1da
Kamil Ziętek <contact@kamilzietek.pl>
2015-05-16 20:44:44 +0200

    Added interaction to view

---
+++ AutolayoutTest/AutolayoutTest/DownloadJsonViewController.h
@@ -0,0 +1,13 @@
+//
+//  DownloadJsonViewController.h
+//  AutolayoutTest
+//
+//  Created by Kamil Zietek on 16.05.2015.
+//  Copyright (c) 2015 Kamil Zietek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownloadJsonViewController : UIViewController
+
+@end
---
+++ AutolayoutTest/AutolayoutTest/DownloadJsonViewController.m
@@ -0,0 +1,37 @@
+//
+//  DownloadJsonViewController.m
+//  AutolayoutTest
+//
+//  Created by Kamil Zietek on 16.05.2015.
+//  Copyright (c) 2015 Kamil Zietek. All rights reserved.
+//
+
+#import "DownloadJsonViewController.h"
+
+@interface DownloadJsonViewController ()
+
+@property(nonatomic,weak) IBOutlet UIImageView *vImage;
+
+@end
+
+@implementation DownloadJsonViewController
+
+#pragma mark - Actions
+
+- (IBAction)actionDownloadAndShow:(UIButton *)sender {
+    
+}
+
+- (IBAction)actionShowFromCache:(UIButton *)sender {
+    
+}
+
+- (IBAction)actionDoYourBest:(UIButton *)sender {
+    
+}
+
+- (IBAction)actionClearCache:(UIButton *)sender {
+    
+}
+
+@end
