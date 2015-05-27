edd5aef1ee79f33725e27ba36b441a7e7e083f9f
Kamil Ziętek <contact@kamilzietek.pl>
2015-05-27 23:48:13 +0200

    Mocks are working

---
+++ AutolayoutTest/AutolayoutTest/ClassB.h
@@ -0,0 +1,15 @@
+//
+//  ClassB.h
+//  AutolayoutTest
+//
+//  Created by Kamil Zietek on 27.05.2015.
+//  Copyright (c) 2015 Kamil Zietek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClassB : NSObject
+
+- (NSString*)respondToMessage:(NSString*)message;
+
+@end
