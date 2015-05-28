acb0a808d3713f36aed57c3f2014102f62d5dc0d
Kamil Ziętek <contact@kamilzietek.pl>
2015-05-28 18:29:46 +0200

    Added verify

--- AutolayoutTest/AutolayoutTestTests/MockTests.m
+++ AutolayoutTest/AutolayoutTestTests/MockTests.m
@@ -57,6 +57,21 @@
     XCTAssert([dataExpected isEqualToString:result], "Data is doubled.");
 }
 
+- (void)testVerify {
+    
+    ClassA *a = [ClassA new];
+    
+    id mockB = OCMClassMock([ClassB class]);
+    
+    a.peer = mockB;
+    NSString *dataSent = @"Pedal";
+    NSString *dataSent2 = [NSString stringWithFormat:@"%@%@",@"Ped",@"al"];
+    
+   [a sendDataAndReceiveResponse:dataSent];
+    
+    OCMVerify([mockB respondToMessage:dataSent2]);
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
